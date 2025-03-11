39dc0fc029e9770ec005991dcd198ded38ea676c
Stefan <git@stefn.co.uk>
2025-03-11 18:23:02 +0000

    Fix RSS feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,38 @@
-const path = require("path");
-const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 const fg = require("fast-glob");
+const path = require("path");
+const { feedPlugin } = require("@11ty/eleventy-plugin-rss");
+const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 
 const images = fg.sync(["src/images/*.jpg"]);
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/**/*");
+
+  eleventyConfig.addPlugin(feedPlugin, {
+    type: "atom",
+    outputPath: "/feed.xml",
+    stylesheet: "/assets/pretty-atom-feed.xsl",
+    templateData: {
+      eleventyNavigation: {
+        key: "Feed",
+        order: 4,
+      },
+    },
+    collection: {
+      name: "post",
+      limit: 20,
+    },
+    metadata: {
+      language: "en",
+      title: "Button Kin Games",
+      subtitle:
+        "Publishing original games and new content for table top roleplaying, aka TTRPGs",
+      base: "https://buttonkin.com/",
+      author: {
+        name: "Yvris",
+      },
+    },
+  });
 
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
     formats: ["webp", "jpeg"],
@@ -19,14 +46,18 @@ module.exports = function (eleventyConfig) {
     },
   });
 
-  eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addPassthroughCopy("src/images");
-  eleventyConfig.addPassthroughCopy({
-    "src/assets/favicon/*": "/",
-  });
+  eleventyConfig
+    .addPassthroughCopy("src/assets")
+    .addPassthroughCopy("src/images")
+    .addPassthroughCopy("src/assets/pretty-atom-feed.xsl")
+    .addPassthroughCopy({ "src/assets/favicon/*": "/" });
 
   eleventyConfig.addCollection("images", (collection) => {
     return images.map((i) => i.split("/")[2]).reverse();
+  });
+
+  eleventyConfig.addFilter("dateToRfc822", function (date) {
+    return new Date(date).toUTCString();
   });
 
   return {
@@ -37,7 +68,7 @@ module.exports = function (eleventyConfig) {
       layouts: "_layouts",
       data: "_data",
     },
-    templateFormats: ["liquid", "md"],
+    templateFormats: ["liquid", "md", "njk"],
     htmlTemplateEngine: "liquid",
     markdownTemplateEngine: "liquid",
   };
--- src/pages/archive.md
+++ src/pages/archive.md
@@ -1,7 +1,8 @@
 ---
-layout: postarchive.html
 cssClass: page--newsletter
 metaTitle: "Button Kin Blog Archive"
 title: "Button Kin Blog Archive"
 subtitle: "Behind the scenes updates, freebies, plus TTRPG news and reviews"
+layout: "archive.html"
+permalink: "/newsletter/"
 ---
