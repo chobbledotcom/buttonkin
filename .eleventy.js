94c3df49602684f1fe22a51114360f0e437878f2
Stefan <git@stefn.co.uk>
2025-03-12 01:51:26 +0000

    More image resolutions

--- .eleventy.js
+++ .eleventy.js
@@ -6,70 +6,70 @@ const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 const images = fg.sync(["src/images/*.jpg"]);
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget("./src/**/*");
+	eleventyConfig.addWatchTarget("./src/**/*");
 
-  eleventyConfig.addPlugin(feedPlugin, {
-    type: "atom",
-    outputPath: "/feed.xml",
-    stylesheet: "/assets/pretty-atom-feed.xsl",
-    templateData: {
-      eleventyNavigation: {
-        key: "Feed",
-        order: 4,
-      },
-    },
-    collection: {
-      name: "post",
-      limit: 20,
-    },
-    metadata: {
-      language: "en",
-      title: "Button Kin Games",
-      subtitle:
-        "Publishing original games and new content for table top roleplaying, aka TTRPGs",
-      base: "https://buttonkin.com/",
-      author: {
-        name: "Yvris",
-      },
-    },
-  });
+	eleventyConfig.addPlugin(feedPlugin, {
+		type: "atom",
+		outputPath: "/feed.xml",
+		stylesheet: "/assets/pretty-atom-feed.xsl",
+		templateData: {
+			eleventyNavigation: {
+				key: "Feed",
+				order: 4,
+			},
+		},
+		collection: {
+			name: "post",
+			limit: 20,
+		},
+		metadata: {
+			language: "en",
+			title: "Button Kin Games",
+			subtitle:
+				"Publishing original games and new content for table top roleplaying, aka TTRPGs",
+			base: "https://buttonkin.com/",
+			author: {
+				name: "Yvris",
+			},
+		},
+	});
 
-  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
-    formats: ["webp", "jpeg"],
-    widths: [200, 400],
-    htmlOptions: {
-      imgAttributes: {
-        loading: "lazy",
-        decoding: "async",
-      },
-      pictureAttributes: {},
-    },
-  });
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+		formats: ["webp", "jpeg"],
+		widths: [200, 400, 800, 1600],
+		htmlOptions: {
+			imgAttributes: {
+				loading: "lazy",
+				decoding: "async",
+			},
+			pictureAttributes: {},
+		},
+	});
 
-  eleventyConfig
-    .addPassthroughCopy("src/assets")
-    .addPassthroughCopy("src/images")
-    .addPassthroughCopy("src/assets/pretty-atom-feed.xsl")
-    .addPassthroughCopy({ "src/assets/favicon/*": "/" });
+	eleventyConfig
+		.addPassthroughCopy("src/assets")
+		.addPassthroughCopy("src/images")
+		.addPassthroughCopy("src/assets/pretty-atom-feed.xsl")
+		.addPassthroughCopy({ "src/assets/favicon/*": "/" });
 
-  eleventyConfig.addCollection("images", (collection) => {
-    return images.map((i) => i.split("/")[2]).reverse();
-  });
+	eleventyConfig.addCollection("images", (collection) => {
+		return images.map((i) => i.split("/")[2]).reverse();
+	});
 
-  eleventyConfig.addFilter("dateToRfc822", function (date) {
-    return new Date(date).toUTCString();
-  });
+	eleventyConfig.addFilter("dateToRfc822", function (date) {
+		return new Date(date).toUTCString();
+	});
 
-  return {
-    dir: {
-      input: "src",
-      output: "_site",
-      includes: "_includes",
-      layouts: "_layouts",
-      data: "_data",
-    },
-    templateFormats: ["liquid", "md", "njk"],
-    htmlTemplateEngine: "liquid",
-    markdownTemplateEngine: "liquid",
-  };
+	return {
+		dir: {
+			input: "src",
+			output: "_site",
+			includes: "_includes",
+			layouts: "_layouts",
+			data: "_data",
+		},
+		templateFormats: ["liquid", "md", "njk"],
+		htmlTemplateEngine: "liquid",
+		markdownTemplateEngine: "liquid",
+	};
 };
